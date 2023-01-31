a7d8f0bbf134591d423ef33f2ea8d2538f514367
Andrey Shishkov <shishkoffandrey@yandex.ru>
2023-02-01 02:43:48 +0300

    Add base class handler

---
+++ handlers/handler.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from markup.markup import Keyboards
+from data_base.dbalchemy import DBManager
+
+
+class Handler(ABC):
+
+    def  __init__(self, bot):
+        self.bot = bot
+        self.keyboards = Keyboards()
+
+    @abstractmethod
+    def handle(self):
+        pass

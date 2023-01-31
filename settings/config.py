b1514edc52d120495cdab9f6d1861a305f6f9362
Andrey Shishkov <shishkoffandrey@yandex.ru>
2023-02-01 02:10:47 +0300

    Add config

---
+++ settings/config.py
@@ -0,0 +1,48 @@
+import os
+from emoji import emojize
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.environ.get('TOKEN')
+
+NAME_DB = 'products.sqlite'
+VERSION = '0.0.1'
+AUTHOR = 'User'
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE = os.path.join('sqlite:///'+BASE_DIR, NAME_DB)
+
+COUNT = 0
+
+KEYBOARD = {
+    'CHOOSE_GOODS': emojize(':open_file_folder: Выбрать товар'),
+    'INFO': emojize(':speech_balloon: О магазине'),
+    'SETTINGS': emojize('⚙️ Настройки'),
+    'SEMIPRODUCT': emojize(':pizza: Полуфабрикаты'),
+    'GROCERY': emojize(':bread: Бакалея'),
+    'ICE_CREAM': emojize(':shaved_ice: Мороженое'),
+    '<<': emojize('⏪'),
+    '>>': emojize('⏩'),
+    'BACK_STEP': emojize('◀️'),
+    'NEXT_STEP': emojize('▶️'),
+    'ORDER': emojize('✅ ЗАКАЗ'),
+    'X': emojize('❌'),
+    'DOUWN': emojize('🔽'),
+    'AMOUNT_PRODUCT': COUNT,
+    'AMOUNT_ORDERS': COUNT,
+    'UP': emojize('🔼'),
+    'APPLAY': '✅ Оформить заказ',
+    'COPY': '©️'
+}
+
+CATEGORY = {
+    'SEMIPRODUCT': 1,
+    'GROCERY': 2,
+    'ICE_CREAM': 3,
+}
+
+COMMANDS = {
+    'START': "start",
+    'HELP': "help",
+}

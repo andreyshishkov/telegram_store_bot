d978c0fe6d1d101e91ef74a26680ab22add08de6
Andrey Shishkov <shishkoffandrey@yandex.ru>
2023-02-02 03:05:53 +0300

    Add com_handler to main handler

--- handlers/handler_main.py
+++ handlers/handler_main.py
@@ -1,7 +1,12 @@
+from handlers.handler_com import HandlerCommands
+
+
 class HandlerMain:
 
     def __init__(self, bot):
         self.bot = bot
 
+        self.handler_commands = HandlerCommands(self.bot)
+
     def handle(self):
-        pass
+        self.handler_commands.handle()

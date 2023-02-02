a28de7fe6ea8456003228cdab3b5dd4edda6883c
Andrey Shishkov <shishkoffandrey@yandex.ru>
2023-02-02 03:03:20 +0300

    Add start_menu

--- markup/markup.py
+++ markup/markup.py
@@ -1,4 +1,4 @@
-from telebot.types import KeyboardButton
+from telebot.types import KeyboardButton, ReplyKeyboardMarkup
 from settings import config
 from data_base.dbalchemy import DBManager
 
@@ -12,3 +12,14 @@ class Keyboards:
     @staticmethod
     def set_btn(name, step=0, quantity=0):
         return KeyboardButton(config.KEYBOARD[name])
+
+    def start_menu(self):
+        self.markup = ReplyKeyboardMarkup(True, True)
+        itm_btn_1 = self.set_btn('CHOOSE_GOODS')
+        itm_btn_2 = self.set_btn('INFO')
+        itm_btn_3 = self.set_btn('SETTINGS')
+
+        self.markup.row(itm_btn_1)
+        self.markup.row(itm_btn_2, itm_btn_3)
+
+        return self.markup
